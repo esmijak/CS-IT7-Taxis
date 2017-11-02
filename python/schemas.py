9fd0d7f5c041e4001e0b05ae3cc2f442113cd0cb
Dennis <dennisdw@outlook.com>
2017-11-02 14:42:58 +0100

    Update schemas.py with more tables

--- python/schemas.py
+++ python/schemas.py
@@ -1,26 +1,54 @@
 from pyspark.sql.types import *
 from enum import Enum, auto
 
-combinedDataSchema = StructType([StructField("ride_id", IntegerType(), False), StructField("pickup_cid", IntegerType(), True), StructField("dropoff_cid", IntegerType(), True), StructField("pickup_timeslot_id", IntegerType(), True), StructField("dropoff_timeslot_id", IntegerType(), True)])
+combinedDataSchema = StructType([
+  StructField("ride_id", IntegerType(), False),
+  StructField("pickup_cid", IntegerType(), True),
+  StructField("dropoff_cid", IntegerType(), True),
+  StructField("pickup_timeslot_id", IntegerType(), True),
+  StructField("dropoff_timeslot_id", IntegerType(), True)])
 
-clusterDataSchema = StructType([StructField("ride_id", IntegerType(), False), StructField("cetroid_long", DoubleType(), True), StructField("centroid_lat", DoubleType(), True)])
+clusterDataSchema = StructType([
+  StructField("ride_id", IntegerType(), False),
+  StructField("cetroid_long", DoubleType(), True),
+  StructField("centroid_lat", DoubleType(), True)])
 
-timeSlotSchema = StructType([StructField("from", TimestampType(), False), StructField("to", TimestampType(), False), StructField("id", IntegerType(), False)])
+timeSlotSchema = StructType([
+  StructField("from", TimestampType(), False),
+  StructField("to", TimestampType(), False),
+  StructField("id", IntegerType(), False)])
 
-rawDataSchema = StructType([StructField("tpep_pickup_datetime", TimestampType(), True), StructField("tpep_dropoff_datetime", TimestampType(), True), StructField("pickup_longitude", DoubleType(), True), StructField("pickup_latitude", DoubleType(), True), StructField("dropoff_longitude", DoubleType(), True), StructField("dropoff_latitude", DoubleType(), True), StructField("id", IntegerType(), False)])
+rawDataSchema = StructType([
+  StructField("tpep_pickup_datetime", TimestampType(), True),
+  StructField("tpep_dropoff_datetime", TimestampType(), True),
+  StructField("pickup_longitude", DoubleType(), True),
+  StructField("pickup_latitude", DoubleType(), True),
+  StructField("dropoff_longitude", DoubleType(), True),
+  StructField("dropoff_latitude", DoubleType(), True),
+  StructField("id", IntegerType(), False)])
 
-demandSchema = StructType([StructField("pickup_timeslot_id", IntegerType(), False), StructField("pickup_cid", IntegerType(), False), StructField("demand", IntegerType(), True)])
+demandSchema = StructType([
+  StructField("pickup_timeslot_id", IntegerType(), False),
+  StructField("pickup_cid", IntegerType(), False),
+  StructField("demand", IntegerType(), True)])
+
+rideClusterSchema = StructType([
+  StructField("ride_id", IntegerType(), False),
+  StructField("pickup_cid", IntegerType(), False),
+  StructField("dropoff_cid", IntegerType(), False)])
 
 class Table(Enum):
   COMBINED_DATA = auto()
   CLUSTER_DATA = auto()
   TIME_SLOTS = auto()
   RAW_DATA = auto()
+  RIDE_CLUSTERS = auto()
 
   COMBINED_DATA_SAMPLE = auto()
   CLUSTER_DATA_SAMPLE = auto()
   RAW_DATA_SAMPLE = auto()
   DEMAND_SAMPLE = auto()
+  RIDE_CLUSTERS_SAMPLE = auto()
 
 def tableName(tab):
   if tab is Table.COMBINED_DATA:
@@ -31,6 +59,8 @@ def tableName(tab):
     return "time_slots"
   elif tab is Table.RAW_DATA:
     return "trips"
+  elif tab is Table.RIDE_CLUSTERS:
+    return "ride_clusters"
   elif tab is Table.COMBINED_DATA_SAMPLE:
     return "combined_data_sample"
   elif tab is Table.CLUSTER_DATA_SAMPLE:
@@ -39,6 +69,8 @@ def tableName(tab):
     return "trips_sample"
   elif tab is Table.DEMAND_SAMPLE:
     return "demand_sample"
+  elif tab is Table.RIDE_CLUSTERS_SAMPLE:
+    return "ride_clusters_sample"
   else:
     return None
 
@@ -53,6 +85,8 @@ def schemaForTable(tab):
     return rawDataSchema
   elif tab is Table.DEMAND_SAMPLE:
     return demandSchema
+  elif tab is Table.RIDE_CLUSTERS or tab is Table.RIDE_CLUSTERS_SAMPLE:
+    return rideClusterSchema
   else:
     return None
 
