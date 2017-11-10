b793b3e6001aa503a8fde1c855a46465f6442f7b
csit7 <433478@mail.muni.cz>
2017-11-10 10:42:10 +0100

    HUGE performance boost for DemandCache

--- python/demand_cache.py
+++ python/demand_cache.py
@@ -1,10 +1,18 @@
 from schemas import registerTable, Table
 from collections import deque
+from concurrent.futures import ThreadPoolExecutor
+from timeit import timeit
 
-CACHED_TIMESLOT_COUNT = 3
-DEMAND_CACHE_DEBUG = False
+CACHED_TIMESLOT_COUNT = 8              # The total number of pages to be fetched, including the 'current page'
 
-# TODO: Make fetching queued pages asynchronous (promises?)
+DEMAND_CACHE_DEBUG = True              # Controls debug prints
+
+DEMAND_CACHE_ASYNC = True              # If true, future pages will be fetched asynchronously
+
+DEMAND_CACHE_UNPACK_DATAFRAMES = True  # If true, dataframes will be converted to a dictionary format.
+                                       # This will make queries faster, but initial fetching slower.
+                                       # If both DEMAND_CACHE_ASYNC and DEMAND_CACHE_HEAVY_FETCH
+                                       # are true, this conversion will be done in the background if possible
 
 """
 DemandCache can be used to efficiently fetch individual values from the demand table.
@@ -31,6 +39,14 @@ After that, you can do these things with the global variable demandCache:
         since it will require rebuilding the cache from scratch. Use this for cleanup in order to prevent
         memory issues.
 
+The constants above this comment can be adjusted, but you probably shouldn't.
+CACHED_TIMESLOT_COUNT can be set higher if you are processing a lot of data, in the proper order.
+DEMAND_CACHE_DEBUG = True will just flood your console.
+DEMAND_CACHE_ASYNC = False will make cache misses much more expensive, while only 
+                           providing a small increase to performance for hits.
+DEMAND_CACHE_UNPACK_DATAFRAMES = False will destroy performance. It only makes sense if you are not processing
+                                       data sequentially, but then you shouldn't be using the cache anyway.
+
 """
 
 class DemandCache:
@@ -38,41 +54,56 @@ class DemandCache:
         def __init__(self, spark, sqlCtx):
             registerTable(sqlCtx, Table.DEMAND)
             self._spark = spark
+            self._exec = ThreadPoolExecutor(max_workers=3)
             self._start_tid = -1
-            self._current_df = None
-            self._next_dfs = deque([])
+            self._current_page = None
+            self._next_pages = deque([])
             if DEMAND_CACHE_DEBUG:
                  print('Demand cache initialized')
 
         def _fetch(self, tid):
-            return self._spark.sql('SELECT * FROM demand WHERE pickup_timeslot_id = {}'.format(tid))
+            res = self._spark.sql('SELECT * FROM demand WHERE pickup_timeslot_id = {}'.format(tid))
+            if DEMAND_CACHE_DEBUG:
+                print("Page {} fetched".format(tid))
+            if DEMAND_CACHE_UNPACK_DATAFRAMES:
+                res = {cid: demand for (_, cid, demand) in res.collect()}
+            return res
 
         def _reinit(self, tid):
             if DEMAND_CACHE_DEBUG:
                 print('Reinitializing cache, fetching tids {} through {}'.format(tid, tid + CACHED_TIMESLOT_COUNT - 1))
             self._start_tid = tid
-            self._current_df = self._fetch(self._start_tid)
+            self._current_page = self._fetch(self._start_tid)
+            for page in self._next_pages:
+                del page
+            self._next_pages = deque([])
             for i in range(1, CACHED_TIMESLOT_COUNT):
-                self._next_dfs.append(self._fetch(tid + i))
+                if DEMAND_CACHE_ASYNC:
+                    self._next_pages.append(self._exec.submit(self._fetch, tid + i))
+                else:
+                    self._next_pages.append(self._fetch(tid + i))
             if DEMAND_CACHE_DEBUG:
-                print('Reinitialization done. tid {} in memory, next {} in queue'.format(tid, len(self._next_dfs)))
+                print('Reinitialization done. tid {} in memory, next {} in queue'.format(tid, len(self._next_pages)))
 
-        def _update_df(self, tid, cid):
-            if self._current_df is None:
+        def _update_page(self, tid, cid):
+            if self._current_page is None:
                 self._reinit(tid)
             elif tid == self._start_tid:
                 if DEMAND_CACHE_DEBUG:
                     print('DemandCache: Cache hit, tid {}'.format(tid))
             elif tid == self._start_tid + 1:
-                if len(self._next_dfs) > 0:
+                if len(self._next_pages) > 0:
                     if DEMAND_CACHE_DEBUG:
                         print('Updating cache: Discarding tid {}, adding {} to the queue'.format(self._start_tid,
                                                                                                  tid + CACHED_TIMESLOT_COUNT - 1))
-                    next = self._next_dfs.popleft()
-                    del self._current_df
-                    self._current_df = next
+                    next = self._next_pages.popleft()
+                    del self._current_page
+                    self._current_page = next.result() if DEMAND_CACHE_ASYNC else next
                     self._start_tid = tid
-                    self._next_dfs.append(self._fetch(tid + CACHED_TIMESLOT_COUNT - 1))
+                    if DEMAND_CACHE_ASYNC:
+                        self._next_pages.append(self._exec.submit(self._fetch, tid + CACHED_TIMESLOT_COUNT - 1))
+                    else:
+                        self._next_pages.append(self._fetch(tid + CACHED_TIMESLOT_COUNT - 1))
                 else:
                     print('DemandCache: Queue exhausted')
                     self._reinit(tid)
@@ -84,19 +115,22 @@ class DemandCache:
                 self._reinit(tid)
 
         def get_demand(self, tid, cid):
-            self._update_df(tid, cid)
-            res = self._current_df.filter('pickup_timeslot_id = {} AND pickup_cid = {}'.format(tid, cid))
-            return res.head()['cnt'] if res.count() > 0 else 0
+            self._update_page(tid, cid)
+            if DEMAND_CACHE_UNPACK_DATAFRAMES:
+                return self._current_page.get(cid, 0)
+            else:
+                res = self._current_page.filter('pickup_timeslot_id = {} AND pickup_cid = {}'.format(tid, cid))
+                return res.head()['cnt'] if res.count() > 0 else 0
 
         def hint(self, tid):
-            self._update_df(tid, 0)
+            self._update_page(tid, 0)
 
         def clear(self):
-            del self._current_df
-            self._current_df = None
-            for df in self._next_dfs:
-                del df
-            self._next_dfs = deque([])
+            del self._current_page
+            self._current_page = None
+            for page in self._next_pages:
+                del page
+            self._next_pages = deque([])
             self._start_tid = -1
 
     instance = None
@@ -108,3 +142,10 @@ class DemandCache:
 
 
 demandCache = DemandCache()
+def demandCacheStressTest(page_count, cluster_count):
+    global demandCache
+    def fn():
+        for tid in range(1, page_count):
+            for cid in range(1, cluster_count):
+                demandCache.get_demand(tid, cid)
+    print(timeit(fn, number=3))
