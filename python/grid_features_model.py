e298c985da3acb0c7806d22638775b3ddc32f55a
Unknown <benzergaamina@gmail.com>
2017-12-13 12:16:58 +0100

    graph and final values for the grid

--- python/grid_features_model.py
+++ python/grid_features_model.py
@@ -16,9 +16,9 @@ sqlCtx = SQLContext(spark.sparkContext, spark)
 #registerTable(sqlCtx, Table.FINAL_FEATURES_GRID)
 
 def get_features_for_grid(sqlCtx, lat, long, split_train_test=True):
-    df = spark.read.parquet(hadoopify('grids/final_features_grid')).filter('pickup_lat_slot = {}'.format(lat)) \
-        .filter('pickup_long_slot = {}'.format(long)).orderBy('pickup_lat_slot') \
-        .drop('pickup_lat_slot', 'pickup_long_slot', 'pickup_timeslot_id')
+    df = spark.read.parquet(hadoopify('grids/final_with_zero/{}_{}'.format(long, lat))) \
+        .orderBy('pickup_lat_slot') \
+        .drop('pickup_timeslot_id')
     #df = loadDataFrame(sqlCtx, Table.FINAL_FEATURES_GRID).filter('pickup_lat_slot = {} and pickup_long_slot = {}'.format(pickup_lat_slot, pickup_long_slot)).orderBy('origin')\
     #    .drop('origin', 'pickup_timeslot_id')
     if split_train_test:
@@ -31,8 +31,8 @@ def get_features_for_grid(sqlCtx, lat, long, split_train_test=True):
 
 def get_features_for_all_grid(sqlCtx, split_train_test=True):
     all = []
-    for x in range (25):
-        for y in range(25):
+    for x in range (24):
+        for y in range(24):
             df = spark.read.parquet(hadoopify('grids/final_features_grid')).filter('pickup_lat_slot = {}'.format(x)) \
                 .filter('pickup_long_slot = {}'.format(y)).orderBy('pickup_lat_slot')
             all.extend(df.collect())
@@ -54,11 +54,13 @@ def doGrid_one():
     errorsR2_DT = []
     hor = grid_data['horizontal_slots']
     vert = grid_data['vertical_slots']
+    print(hor, vert)
+    hor = 24
+    vert = 24
     for x in range(hor):
+        print('grid hor:', x)
         for y in range(vert):
             train, test = get_features_for_grid(spark, x, y)
-            train.show()
-            test.show()
             assembler = VectorAssembler(inputCols=["day","day_of_week", "hour", "is_airport","is_manhattan","minute", 'pickup_lat_slot', 'pickup_long_slot', "time_of_day_code", "week"],
                                         outputCol='features')
             output_training = assembler.transform(train)
@@ -67,9 +69,14 @@ def doGrid_one():
             final_data_training = output_training.select('features', 'amount')
             final_data_testing = output_testing.select('features', 'amount')
 
+            final_data_training.describe().show()
+            final_data_testing.describe().show()
+            
+
             decisionTree = DecisionTreeRegressor(labelCol='amount', maxDepth=3)
             dt_model = decisionTree.fit(final_data_training)
             predictionsDT = dt_model.transform(final_data_testing)
+            print(dt_model.toDebugString)
 
             linearRegression = LinearRegression(labelCol='amount')
             lr_model = linearRegression.fit(final_data_training)
@@ -78,26 +85,26 @@ def doGrid_one():
             """ Evaluation LR : """
             rmse = predictionsLR.rootMeanSquaredError
             errorsRMSE_LR.append(rmse)
-            print("Root Mean Squared Error (RMSE) for LR on test data = ", rmse)
+            #print("Root Mean Squared Error (RMSE) for LR on test data = ", rmse)
 
             r2 = predictionsLR.r2
             errorsR2_LR.append(r2)
-            print("R Squared Error (R2) for LR on test data = ", r2)
+            #print("R Squared Error (R2) for LR on test data = ", r2)
 
             """ Evaluation DT : """
             evaluatorRMSE = RegressionEvaluator(labelCol="amount", predictionCol="prediction", metricName="rmse")
             rmse = evaluatorRMSE.evaluate(predictionsDT)
             errorsRMSE_DT.append(rmse)
-            print("Root Mean Squared Error (RMSE) for DT on test data = ", rmse)
+            #print("Root Mean Squared Error (RMSE) for DT on test data = ", rmse)
 
             evaluatorR2 = RegressionEvaluator(labelCol="amount", predictionCol="prediction", metricName="r2")
             r2 = evaluatorR2.evaluate(predictionsDT)
             errorsR2_DT.append(r2)
-            print("R Squared Error (R2) for DT on test data = ", r2)
+            #print("R Squared Error (R2) for DT on test data = ", r2)
 
     return hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR
 
-hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR = doGrid_one()
+
 
 
 def write_in_files (hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR, one) :
@@ -124,7 +131,7 @@ def write_in_files (hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errors
         for y in range(vert):
             file_rmse_dt.write("RMSE for grid : (" + str(x)+', '+ str(y) + ") is " + str(errorsRMSE_DT[errorIndex]) + "\n")
             file_r2_dt.write("R2 for grid : (" + str(x)+', '+ str(y) + ") is " + str(errorsR2_DT[errorIndex]) + "\n")
-            file_rmse_lr.write("RMSE for grid : (" + str(x)+', '+ str(y) + ") is " + str([errorIndex]) + "\n")
+            file_rmse_lr.write("RMSE for grid : (" + str(x)+', '+ str(y) + ") is " + str(errorsRMSE_LR[errorIndex]) + "\n")
             file_r2_lr.write("R2 for grid : (" + str(x)+', '+ str(y) + ") is " + str(errorsR2_LR[errorIndex]) + "\n")
             errorIndex += 1
 
@@ -133,7 +140,7 @@ def write_in_files (hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errors
     file_r2_dt.close()
     file_rmse_lr.close()
     file_r2_lr.close()
-
+hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR = doGrid_one()
 write_in_files (hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR, "")
 
 def doGrid_all():
@@ -142,50 +149,59 @@ def doGrid_all():
     errorsR2_LR = []
     errorsRMSE_DT = []
     errorsR2_DT = []
-    hor = grid_data['horizontal_slots']
-    vert = grid_data['vertical_slots']
+    #hor = grid_data['horizontal_slots']
+    #vert = grid_data['vertical_slots']
+    print('before')
+    train, test = get_features_for_all_grid(spark)
+    assembler = VectorAssembler(
+        inputCols=["day","day_of_week", "hour", "is_airport","is_manhattan","minute", 'pickup_lat_slot', 'pickup_long_slot', "time_of_day_code", "week"],
+        outputCol='features')
+    print('after')
+    output_training = assembler.transform(train)
+    output_testing = assembler.transform(test)
+
+    final_data_training = output_training.select('features', 'amount')
+    final_data_testing = output_testing.select('features', 'amount')
+
+    print('before dt')
+    decisionTree = DecisionTreeRegressor(labelCol='amount', maxDepth=3)
+    dt_model = decisionTree.fit(final_data_training)
+    print('before lr')
+    linearRegression = LinearRegression(labelCol='amount')
+    lr_model = linearRegression.fit(final_data_training)
+
+    hor = 24
+    vert = 24
     for x in range(hor):
+        print('grid hor:', x)
         for y in range(vert):
-            train, test = get_features_for_all_grid(spark)
-            train.show()
-            test.show()
-            assembler = VectorAssembler(inputCols=["time_of_day_code", "day_of_week", "day", "week", "hour", "minute", "is_manhattan", "is_airport"],
-                                        outputCol='features')
-            output_training = assembler.transform(train)
-            output_testing = assembler.transform(test)
-
-            final_data_training = output_training.select('features', 'amount')
-            final_data_testing = output_testing.select('features', 'amount')
-
-            decisionTree = DecisionTreeRegressor(labelCol='amount', maxDepth=3)
-            dt_model = decisionTree.fit(final_data_training)
-            predictionsDT = dt_model.transform(final_data_testing)
-
-            linearRegression = LinearRegression(labelCol='amount')
-            lr_model = linearRegression.fit(final_data_training)
-            predictionsLR = lr_model.evaluate(final_data_testing)
+            final_data_testing.show()
+            final_data_testing_grid = final_data_testing.filter(final_data_testing.pickup_long_slot == vert & final_data_testing.pickup_lat_slot == hor)
+            print(type(final_data_testing_grid))
+            predictionsDT = dt_model.transform(final_data_testing_grid)
+            predictionsLR = lr_model.evaluate(final_data_testing_grid)
 
             """ Evaluation LR : """
             rmse = predictionsLR.rootMeanSquaredError
             errorsRMSE_LR.append(rmse)
-            print("Root Mean Squared Error (RMSE) for LR on test data = ", rmse)
+            #print("Root Mean Squared Error (RMSE) for LR on test data = ", rmse)
 
             r2 = predictionsLR.r2
             errorsR2_LR.append(r2)
-            print("R Squared Error (R2) for LR on test data = ", r2)
+            #print("R Squared Error (R2) for LR on test data = ", r2)
 
             """ Evaluation DT : """
             evaluatorRMSE = RegressionEvaluator(labelCol="amount", predictionCol="prediction", metricName="rmse")
             rmse = evaluatorRMSE.evaluate(predictionsDT)
             errorsRMSE_DT.append(rmse)
-            print("Root Mean Squared Error (RMSE) for DT on test data = ", rmse)
+            #print("Root Mean Squared Error (RMSE) for DT on test data = ", rmse)
 
             evaluatorR2 = RegressionEvaluator(labelCol="amount", predictionCol="prediction", metricName="r2")
             r2 = evaluatorR2.evaluate(predictionsDT)
             errorsR2_DT.append(r2)
-            print("R Squared Error (R2) for DT on test data = ", r2)
+            #print("R Squared Error (R2) for DT on test data = ", r2)
 
     return hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR
 
-hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR = doGrid_all()
-write_in_files (hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR, "_one_model")
+#hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR = doGrid_all()
+#write_in_files (hor, vert, errorsR2_DT, errorsRMSE_DT, errorsRMSE_LR, errorsR2_LR, "_one_model")
